61873fa93cd31b150edbeba6132240287935c081
asbin007 <asbingamer@gmail.com>
2025-06-05 19:53:17 +0545

    Enables CORS for all origins
    Adds CORS middleware to allow requests from any origin.
    This is required to allow the frontend application to
    access the backend API from different domains.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import './database/connection'
 import userRoute from './routes/userRoutes'
-
+import cors from 'cors'
 
 const app=express()
 app.use(express.json())
+
+app.use(cors({
+    origin: '*',
+    
+}))
 
 
 app.use('/api/auth',userRoute)
